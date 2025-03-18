548c69715264208f9618893b622379ee957393c7
Amar AA <amar.ashfaq95@gmail.com>
2025-03-18 22:00:36 +0000

    Implemented the checkbox toggle feature for the completion of a task item which updates the database

--- src/pages/TaskList.js
+++ src/pages/TaskList.js
@@ -3,7 +3,43 @@ import { Link } from 'react-router-dom';
 
 function TaskList() {
 
-    const [taskItems, setTaskItems] = useState([]);
+    const [taskItems, setTaskItems] = useState([]); // for an array
+    const [taskItem, setTaskItem] = useState(null); // for an object
+
+    function handleCheckboxChange(taskId) {
+        console.log(`Selected checkbox task id: ${taskId}`)
+
+        let taskItem = taskItems.find((element) => element.id === taskId);
+
+        // Toggle the isCompleted status
+        taskItem.isCompleted = !taskItem.isCompleted;
+
+        setTaskItem(taskItem);
+    }
+
+    useEffect(() => {
+        if (taskItem) {
+            fetch(`https://localhost:7130/api/Tasks/${taskItem.id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ title: taskItem.title, 
+                                        description: taskItem.description, 
+                                        isCompleted: taskItem.isCompleted 
+                })
+            })
+            .then(response => {
+                    if (!response.ok) {
+                        throw new Error("Something went wrong with the response!");
+                    }
+                    return response.json();
+            })
+            .then(data => {
+                    console.log("Updated task:", data);
+                    setTaskItems(prevItems => prevItems.map(item => item.id === data.id ? data : item))
+            })
+            .catch(error => console.error('Error updating task:', error));
+        }
+   }, [taskItem])
 
     useEffect(() => {
         fetch("https://localhost:7130/api/Tasks")
@@ -30,7 +66,14 @@ function TaskList() {
                 {taskItems.map((task) => (
                     <li key={task.id}>
                         {task.title} |
-                        <Link to={`/tasks/${task.id}`}> View Details</Link>                         
+                        <Link to={`/tasks/${task.id}`}> View Details</Link> |
+                        <label htmlFor={`taskItem${task.id}`}> Completed</label>
+                        <input type="checkbox"
+                               id={`taskItem${task.id}`}
+                               name={`taskItem${task.id}`}
+                               checked={task.isCompleted}
+                               onChange={() => handleCheckboxChange(task.id)}
+                        />
                     </li>
                 ))}
             </ul>
